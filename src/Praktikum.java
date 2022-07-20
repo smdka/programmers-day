7ce552a657aad1ff0c9df3ac37266dbe445b1927
Ilia <golovachevie@holmogory.fad.ru>
2022-07-20 16:46:57 +0300

    Initial commit

---
+++ src/Praktikum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Praktikum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year); // високосный
+        } else {
+            System.out.println("13.09." + year); // не високосный
+        }
+    }
+
+    public static boolean isLeapYear(int year) { // здесь нужно определить, является ли переданный год високосным
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else return year % 100 != 0;
+    }
+} 
